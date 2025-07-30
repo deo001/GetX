bb176a37c5f99e39b5764f2f1b71f16c5f5e5f3d
deogratius kibiki <deocoder001@gmail.com>
2025-07-30 22:22:16 +0300

    Get X Learning

---
+++ lib/controllers/post_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class postController extends GetxController{
+  List post = ['Post 1', 'Post 2'].obs;
+}
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:getx/screens/second_page.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("D A S H B O A R D"),
+        leading: IconButton(
+          onPressed: () {
+            // Navigator.pop(context);
+          },
+          icon: Icon(Icons.menu),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  Get.to(SecondPage());
+                },
+                child: Text(" d a t a "),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
